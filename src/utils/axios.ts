c5a0dd42db2b956543e1ca8ff706318f0ff0c26b
Victor Martins <victor2007azevedo@hotmail.com>
2023-10-02 13:51:33 -0300

    URL update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://niver.magi.zip/api/",
+    baseURL: "https://niver-api.fluma.dev/api",
 });
 
 api.interceptors.request.use((config) => {
